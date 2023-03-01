5ae1f2f9d41f2ae1439ee1b56a00813b8efc8f6d
taniaLipiena8 <tanialipiena8@gmail.com>
2023-03-01 14:23:43 +0700

    fixing cart height

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -47,16 +47,16 @@ const Cart = () => {
             <div className="row ">
                 {cart.map((cartItem) => (
                     <div key={cartItem.id} className="col-md-4 my-2">
-                        <div className="card shadow-sm" style={{ height: '350px' }}>
+                        <div className="card shadow-sm" style={{ height: '250px' }}>
                             <div className="card-body">
                                 <div className="row">
                                     <div className="col">
                                         <img src={cartItem.image_m} />
                                     </div>
                                     <div className="col">
-                                        <h3 className="card-subtitle mb-2 text-left">
+                                        <h5 className="card-subtitle mb-2 text-left">
                                             {cartItem.title}
-                                        </h3>
+                                        </h5>
                                         <p className="card-text">sisa stok: {cartItem.stok}</p>
                                         <button className="deleteBtn" onClick={() => handleDelete(cartItem.id)}>delete</button>
                                     </div>
